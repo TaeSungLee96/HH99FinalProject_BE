040bea71d8c22bfa5c8dd094833a5750e0b2a1ca
qwe1234k <bbyl0124@gmail.com>
2022-05-12 14:31:46 +0900

    구글 소셜로그인 "Access-Control-Allow-Origin" 추가

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 const googleCallback = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
   passport.authenticate(
     "google",
     { failureRedirect: "/" },
