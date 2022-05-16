287c2d9f0a1b39fd4cd1e783daed3e250d70d1f8
qwe1234k <bbyl0124@gmail.com>
2022-05-16 11:29:51 +0900

    countryId 삭제 디버깅중

--- routes/subMainPage2.js
+++ routes/subMainPage2.js
@@ -94,6 +94,7 @@ router.get("/filtering/country", async (req, res) => {
     });
     delete countryList[0].info.Bank;
     console.log(delete countryList[0].info.countryId);
+    console.log("wpqkf", countryList[0].info.countryId);
     console.log("삭제 후_cId", countryList[0].info.countryId);
     console.log("삭제 후_Bank", countryList[0].info.Bank);
 
