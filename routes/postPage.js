908125cf021453ccbed079151db96c49cf5cee64
qwe1234k <bbyl0124@gmail.com>
2022-05-17 16:56:11 +0900

    postId 로그 추가

--- routes/postPage.js
+++ routes/postPage.js
@@ -396,6 +396,7 @@ router.patch(
 router.delete("/delete", authMiddleWare, async (req, res) => {
   try {
     let { postId } = req.body;
+    console.log("###postId입니다.", postId);
     const { userInfo } = res.locals;
     const { userId, userName } = userInfo;
 
