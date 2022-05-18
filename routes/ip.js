1fa50b1fbab9c51450f68e2820549e32567ad199
qwe1234k <bbyl0124@gmail.com>
2022-05-18 14:07:52 +0900

    IP 전체 삭제 라우터 추가

--- routes/ip.js
+++ routes/ip.js
@@ -5,7 +5,9 @@ const { Ip } = require("../models");
 // FE가 정각마다 해당 API 호출하기
 router.delete("/delete", async (req, res) => {
   try {
-    await Ip.destroy();
+    await Ip.destroy({
+      attribute: ["ip"],
+    });
     res.status(200).json({ msg: "Ip 전체삭제 완료" });
   } catch (error) {
     console.log(error);
