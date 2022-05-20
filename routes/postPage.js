0f7af635439fed01f25330e365a50b5d81395328
Jiwoongyoo1 <dbwldnd89@gamil.com>
2022-05-20 20:40:03 +0900

    multer-s3테스트

--- routes/postPage.js
+++ routes/postPage.js
@@ -9,6 +9,7 @@ const AWS = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 AWS.config.loadFromPath(path.join(__dirname, "../config/s3.json")); // 인증
+let s3 = new AWS.S3();
 let upload = multer({
   storage: multerS3({
     s3: s3,
