11dcbfa974cfd9eb1cb0ed7b072d758079d8acf4
qwe1234k <bbyl0124@gmail.com>
2022-05-21 22:58:37 +0900

    ss

--- app.js
+++ app.js
@@ -8,39 +8,39 @@ const helmet = require("helmet");
 
 // https 기본 모듈들 불러오기
 const fs = require("fs");
-// const http = require("http");
-// const https = require("https");
+const http = require("http");
+const https = require("https");
 const app = express();
-// const app_low = express();
+const app_low = express();
 
 // 인증서 불러오기
-// const privateKey = fs.readFileSync(__dirname + "/a-fo-back_shop.key", "utf-8");
-// const certificate = fs.readFileSync(
-//   __dirname + "/a-fo-back_shop__crt.pem",
-//   "utf-8"
-// );
-// const ca = fs.readFileSync(__dirname + "/a-fo-back_shop__ca.pem", "utf-8");
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
+const privateKey = fs.readFileSync(__dirname + "/a-fo-back_shop.key", "utf-8");
+const certificate = fs.readFileSync(
+  __dirname + "/a-fo-back_shop__crt.pem",
+  "utf-8"
+);
+const ca = fs.readFileSync(__dirname + "/a-fo-back_shop__ca.pem", "utf-8");
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 // 포트설정
-// const httpPort = 3000;
-// const httpsPort = 443;
+const httpPort = 3000;
+const httpsPort = 443;
 
 // https 리다이렉션 하기
 // app_low : http전용 미들웨어
-// app_low.use((req, res, next) => {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     const to = `https://${req.hostname}:${httpsPort}${req.url}`;
-//     console.log(to);
-//     res.redirect(to);
-//   }
-// });
+app_low.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    const to = `https://${req.hostname}:${httpsPort}${req.url}`;
+    console.log(to);
+    res.redirect(to);
+  }
+});
 
 //라우터 불러오기
 const mainPage = require("./routes/mainPage");
@@ -109,11 +109,11 @@ app.use("/oauth", [auth]);
 app.use("/DBdataInput", [DBdataInput]);
 app.use("/ip", [ipDelete]);
 
-app.listen(3000, () => console.log("start.."));
-// http.createServer(app_low).listen(httpPort, () => {
-//   console.log("http서버가 켜졌습니다.");
-// });
+// app.listen(3000, () => console.log("start.."));
+http.createServer(app_low).listen(httpPort, () => {
+  console.log("http서버가 켜졌습니다.");
+});
 
-// https.createServer(credentials, app).listen(httpsPort, () => {
-//   console.log("https서버가 켜졌습니다.!!");
-// });
+https.createServer(credentials, app).listen(httpsPort, () => {
+  console.log("https서버가 켜졌습니다.!!");
+});
