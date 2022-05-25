84c8aea300bf84e12f96349cdfce0c1c0df86943
qwe1234k <bbyl0124@gmail.com>
2022-05-26 01:10:32 +0900

    미들 웨어 로그추가

--- middleware/authMiddleWare.js
+++ middleware/authMiddleWare.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
   try {
     const token = jwt.verify(logInToken, process.env.key);
     const { userId } = token;
-
+    console.log("여기 온다");
     User.findOne({
       logging: false,
       where: { userId },
