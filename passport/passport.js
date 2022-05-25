c56f7191584ab37b2f1f66867abd405e22977bcc
IcandoDeveloper <96129738+IcandoDeveloper@users.noreply.github.com>
2022-05-25 21:13:10 +0900

    구글로그인 패스포트 수정

--- passport/passport.js
+++ passport/passport.js
@@ -11,7 +11,6 @@ passport.use(
       clientSecret: process.env.GOOGLE_SECRET,
       // 여기는 프론트 주소 넣는곳임 맨뒤에 / 붙이는걸 추천함
       callbackURL: "https://localhost:3000/oauth/google/callback/",
-      passReqToCallback: true,
     },
     async function (accessToken, refreshToken, profile, done) {
       console.log("profile: ", profile.id);
